c9d4da1e21e1f1fa30bb98eb5a3f87e6c7c0c507
daehee.kim <clonekim@gmail.com>
2020-10-05 12:30:28 +0900

    deps update

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 1. Todo
 
 
-## 참고
+## 읽을거리
 [카카오기술블로그](https://tech.kakao.com/search/clojure)  
 [함수형 프로그래밍 언어에 대한 고찰](https://engineering.linecorp.com/ko/blog/functional-programing-language-and-line-game-cloud/)  
 [그외 클로저를 사용하는 회사](https://clojure.org/community/companies)  
--- graphql/project.clj
+++ graphql/project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/tools.logging "1.1.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [org.clojure/data.json "1.0.0"]
                  [integrant "0.8.0"]
-                 [http-kit "2.4.0"]
+                 [http-kit "2.5.0"]
                  [compojure "1.6.2"]
                  [ring/ring-core "1.8.1"]
                  [ring/ring-defaults "0.3.2"]
